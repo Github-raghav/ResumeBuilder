6b6faf1b12b24086807e694fc71a0c7d5219ebbe
Raghav Arora <araghav621@gmail.com>
2021-08-16 12:12:16 +0530

    https://github.com/Github-raghav/ResumeBuilder

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import React,{useEffect} from 'react';
+import { firebaseDb } from './firebase/firebaseConfig';
+import Header from "./Components/Header"
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Home from './Components/Home';
+import AboutUs from './Components/AboutUs';
+import GettingStarted from "./Components/GettingStarted"
+import Contact from './Components/Contact';
+import Education from './Components/Education';
+import Final from './Components/Final';
 
 function App() {
+
+  useEffect(() => {
+    firebaseDb.collection("resumes").get().then(alldocs=>{
+      alldocs.forEach(doc =>{
+        console.log(doc.data());
+      })
+    })
+  }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <h1>app</h1> */}
+      <Router>
+        <Header/>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/aboutus"exact component={AboutUs} />
+          <Route path="/gettingStarted" exact component={GettingStarted}/>
+          <Route path="/contact" exact component={Contact} />
+          <Route path="/education" exact component={Education}/>
+          <Route path="/final" exact component={Final}/>
+        </Switch>
+      </Router>
     </div>
   );
 }
